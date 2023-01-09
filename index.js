900d717e5cb0f166b8462a94a707d6cc9de86895
João Ribeiro <106286628+joaoevribeiro@users.noreply.github.com>
2023-01-09 21:33:03 +0000

    Get DOM elements from index.html

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// Score
+let scoreHomeEl = document.getElementById("score-home");
+let scoreGuestEl = document.getElementById("score-guest");
+
+// Home
+let addOneHomeEl = document.getElementById("add-one-home");
+let addTwoHomeEl = document.getElementById("add-two-home");
+let addThreeHomeEl = document.getElementById("add-three-home");
+let resultHome = 0;
+
+// Guest
+let addOneGuestEl = document.getElementById("add-one-guest");
+let addTwoGuestEl = document.getElementById("add-two-guest");
+let addThreeGuestEl = document.getElementById("add-three-guest");
+let resultGuest = 0;
